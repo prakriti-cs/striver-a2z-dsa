4703b0be7bf0d36b73e7e201aed0e227caf622f9
prakriti-cs <pihoosingh755@gmail.com>
2026-01-25 16:08:00 +0530

    Added C++ practice files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    string s = 
 
-}   
+#include <iostream>
+using namespace std;
+
+void print(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5;
+    print(n);
+    return 0;
+}
